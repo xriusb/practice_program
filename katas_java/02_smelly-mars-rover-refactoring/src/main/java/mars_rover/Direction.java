6c1f35e61ec68e1c7238fc18898aec223257f96d
xriusb <xriusb@uoc.edu>
2020-01-21 22:08:13 +0100

    refactor of the Direction create method

--- katas_java/02_smelly-mars-rover-refactoring/src/main/java/mars_rover/Direction.java
+++ katas_java/02_smelly-mars-rover-refactoring/src/main/java/mars_rover/Direction.java
@@ -4,17 +4,10 @@ public enum Direction {
     N, S, E, W;
 
     public static Direction create(String directionEncoding) {
-        if ( "S".equals(directionEncoding)) {
-            return S;
-        }
-        if ( "E".equals(directionEncoding)) {
-            return E;
-        }
-        if ( "W".equals(directionEncoding)) {
-            return W;
-        }
-        if ( "N".equals(directionEncoding)) {
-            return N;
+        for(Direction d : Direction.values()){
+            if(d.name().equals(directionEncoding)){
+                return Direction.valueOf(directionEncoding);
+            }
         }
         throw new IllegalArgumentException();
     }
